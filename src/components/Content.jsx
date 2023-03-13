caf7c97e8bb1e7b94a0071e42c5c46943de6f482
rafikusdiarto <rafikusdiarto1707@gmail.com>
2023-03-13 16:05:59 +0700

    add home components

---
+++ src/components/Content.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function Content() {
+  return (
+    <div>
+        <section className="text-gray-600 body-font">
+            <div className="container px-5 py-24 mx-auto flex flex-wrap">
+                <h2 className="sm:text-3xl text-2xl text-gray-900 font-medium title-font mb-2 md:w-2/5">Kickstarter Actually Pinterest Brunch Bitters Occupy</h2>
+                <div className="md:w-3/5 md:pl-6">
+                <p className="leading-relaxed text-base">Taxidermy bushwick celiac master cleanse microdosing seitan. Fashion axe four dollar toast truffaut, direct trade kombucha brunch williamsburg keffiyeh gastropub tousled squid meh taiyaki drinking vinegar tacos.</p>
+                <div className="flex md:mt-4 mt-6">
+                    <button className="inline-flex text-white bg-indigo-500 border-0 py-1 px-4 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+                    <a className="text-indigo-500 inline-flex items-center ml-4">Learn More
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                    </a>
+                </div>
+                </div>
+            </div>
+
+            <div class="container px-6 py-16 mx-auto text-center">
+        
+
+        <div class="flex justify-center mt-10">
+            <img class="object-cover w-full h-96 rounded-xl lg:w-4/5" src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80" />
+        </div>
+        <div class="max-w-lg mx-auto">
+            <h1 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Building Your Next App with our Awesome components</h1>
+            <p class="mt-6 text-gray-500 dark:text-gray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero similique
+                obcaecati illum mollitia.</p>
+            <button class="px-5 py-2 mt-6 text-sm font-medium leading-5 text-center text-white capitalize bg-blue-600 rounded-lg hover:bg-blue-500 lg:mx-0 lg:w-auto focus:outline-none">
+                Enjoy Our Products
+            </button>
+            <p class="mt-3 text-sm text-gray-400 ">No credit card required</p>
+        </div>
+    </div>
+        </section>
+
+        
+    </div>
+  )
+}
